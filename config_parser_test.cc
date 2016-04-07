4c823d83d0e18d9b9b0128dfbe8f3c4c9d19e246
dinglis <dinglis@caltech.edu>
2016-04-06 20:47:10 -0700

    added parser tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,26 @@
+#include <sstream>
+#include <string>
+
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+class NginxStringConfigTest : public ::testing::Test {
+    protected:
+        bool ParseString(const std::string &config_string) {
+            std::stringstream config_stream(config_string);
+            return parser_.Parse(&config_stream, &config_);
+        }
+        NginxConfigParser parser_;
+        NginxConfig config_;
 
-  bool success = parser.Parse("example_config", &out_config);
+};
 
-  EXPECT_TRUE(success);
+TEST_F(NginxStringConfigTest, SimpleConfig) {
+    EXPECT_TRUE(ParseString("foo bar;")) << "error!";
+    EXPECT_EQ(1, config_.statements_.size()) << "error!";
+    EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "error";
+}
+
+TEST_F(NginxStringConfigTest, NestedConfig) {
+    EXPECT_TRUE(ParseString("server { listen 80; }"));
 }
