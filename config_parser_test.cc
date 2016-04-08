118301f9e9e9ab91e254fe39130ddd733727a2c6
dinglis <dinglis@caltech.edu>
2016-04-07 22:41:31 -0700

    fixed curly brace parsing errors; added test coverage

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -15,12 +15,20 @@ class NginxStringConfigTest : public ::testing::Test {
 
 };
 
-TEST_F(NginxStringConfigTest, SimpleConfig) {
-    EXPECT_TRUE(ParseString("foo bar;")) << "error!";
-    EXPECT_EQ(1, config_.statements_.size()) << "error!";
-    EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "error";
+TEST_F(NginxStringConfigTest, SyntaxErrorConfig) {
+    EXPECT_FALSE(ParseString("foo bar")) << "Error: failed to detect missing semicolon.";
+    EXPECT_FALSE(ParseString("server { listen 80;")) << "Error: failed to detect missing }";
+    EXPECT_FALSE(ParseString("listen 80; }")) << "Error: failed to detect missing {";
+    EXPECT_FALSE(ParseString("server {listen 80 }")) << "Error: failed to detect missing semicolon within braces";
+    EXPECT_FALSE(ParseString("server { \n server { listen 80; }")) << "Error: failed to detect error in nested braces";
 }
 
-TEST_F(NginxStringConfigTest, NestedConfig) {
-    EXPECT_TRUE(ParseString("server { listen 80; }"));
+TEST_F(NginxStringConfigTest, ValidSyntaxConfig) {
+    EXPECT_TRUE(ParseString("foo bar;")) << "Error: failed to parse valid string correctly";
+    EXPECT_EQ(1, config_.statements_.size()) << "Error: parsed token into incorrect number of statements";
+    EXPECT_EQ("foo", config_.statements_.at(0)->tokens_.at(0)) << "Error: tokenized statement incorrectly";
+}
+
+TEST_F(NginxStringConfigTest, DoubleNestedConfig) {
+    EXPECT_TRUE(ParseString("server { \n server { listen 80; } \n }")) << "Error: failed to parse doubly nested braces correctly";
 }
